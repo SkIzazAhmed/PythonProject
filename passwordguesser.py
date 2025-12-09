9f9c45f9ae7334d46fd8bc587e2ad43591e4862a
SkIzazAhmed <2102508792@svyasa-sas.edu.in>
2025-12-09 14:54:36 +0530

    Python Project

---
+++ passwordguesser.py
@@ -0,0 +1,28 @@
+import random
+
+
+target = list(input("Enter the target word: "))
+len1=len(target)
+
+current = list("a"*len1)
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet=alphabet+alphabet.upper()
+
+attempts = 0
+
+
+def get_random_char():
+
+    return random.choice(alphabet)
+
+for i in range(len1):
+    while True:
+        if(current[i]==target[i]):
+            break
+        else:
+            r=get_random_char()
+            current[i]=r
+        print("".join(current))
+
+
